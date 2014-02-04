168698f728458d50fd4275eac7fbe755ed805504
Steven Holms <superlinkx@gmail.com>
2014-02-04 04:19:52 -0800

    Reorg

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+  chrome.app.window.create('../index.html', {
     width: 480,
     height: 800,
   });
